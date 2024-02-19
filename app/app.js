f948c68c9f913796080cf7bf645f283d44d8c4b1
rabinacharya11 <charms2074@gmail.com>
2024-02-19 09:34:13 +0545

    added server and listeners

---
+++ app/app.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const cors  = require('cors'); 
+const app = express();  
+
+// use middleware to form the contract for incoming requests
+app.use(express.json());  
+
+// use middleware for url encoding
+
+app.use(express.urlencoded({ extended: true })); 
+
+// use handle middleware to handle core policy 
+
+app.use(cors()); 
+
+app.get('/', (req, res, next) => {    
+    res.status(200).json({ message: 'Welcome to the API' });    
+}
+); 
+
+// routers 
+
+
+
+/// bad url or error handler 
+/// with middleware
+
+app.use((req, res, next) => { 
+
+    const error = new Error('Not found'); 
+    error.status = 404; 
+    next(error);  
+
+}); 
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500); 
+    res.json({
+        error: {
+            message: error.message,
+            status : error.status
+        }
+    }); 
+}
+);
+
+
+
+module.exports = app;  
